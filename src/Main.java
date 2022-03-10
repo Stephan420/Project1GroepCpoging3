142d381dd7d8c1a68ad6e7bbe2d3bcc8f30ddb15
Deevvje <devikarose@live.nl>
2022-03-10 15:04:39 +0100

    Dank memes
    push

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("hallo wereld");
         System.out.println("Gaat het dan eindelijk lukken?");
         System.out.println("lukken is gelukt");
+        System.out.println("dank memes");
     }
 }
