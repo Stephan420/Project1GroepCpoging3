a11fa7069010eccb1dbfa7116b1b3e6599e9780a
ramazan2duizend <ramazan2000@live.nl>
2022-03-17 14:26:32 +0100

    Menu toegevoegd
    Het menu is er

--- src/Main.java
+++ src/Main.java
@@ -58,8 +58,17 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Examens> vragen = new ArrayList<>();
 
-
-
+        System.out.println("Menu");
+        System.out.println("1) Lijst met examens");
+        System.out.println("2) Lijst met studenten");
+        System.out.println("3) Nieuwe student inschrijven");
+        System.out.println("4) Student verwijderen");
+        System.out.println("5) Examen afnemen");
+        System.out.println("6) Is student geslaagd voor test?");
+        System.out.println("7) Welke examens heeft student gehaald?");
+        System.out.println("8) Welke student heeft de meeste examens gehaald?");
+        System.out.println("0) Exit");
+        System.out.println("Kies een getal van het menu: ");
 
     }
 }
