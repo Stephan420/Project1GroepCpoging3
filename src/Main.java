eabc7ee7d6e7d4952f97fbe32ba918f5a6730835
Deevvje <devikarose@live.nl>
2022-03-22 10:55:06 +0100

    object examen aangemaakt

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,31 @@ class Examens {
     int aantalvragen;
     ArrayList<Vraag> alleVragen = initialiseerExamenVragen();
 
+    public Examens(String naam, int aantalvragen, ArrayList<Vraag> alleVragen){
+
+        this.naam = naam;
+        this.aantalvragen = aantalvragen;
+        this.alleVragen = alleVragen;
+    }
+
     static ArrayList<Vraag>initialiseerExamenVragen(){
         ArrayList<Vraag> alleVragen = new ArrayList<Vraag>();
         Vraag vraag1AK = new Vraag("Is de aarde plat?", false);
-        Vraag vraag2AK = new Vraag("Weegt de aarde 5,972E24 kg?", false);
-        Vraag vraag3AK = new Vraag("Is pluto een planeet?", false);
+        Vraag vraag2AK = new Vraag("Stoot waterkracht CO2 uit?", false);
+        Vraag vraag3AK = new Vraag("Is steenkool een metamorf gesteente?", false);
+        Vraag vraag4AK = new Vraag("Stoot biomassa CO2 uit als je het verbrandt?", true);
+        Vraag vraag5AK = new Vraag("De langste plaatsnaam ter wereld heet 'Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch'", true);
         alleVragen.add(vraag1AK);
         alleVragen.add(vraag2AK);
         alleVragen.add(vraag3AK);
+        alleVragen.add(vraag4AK);
+        alleVragen.add(vraag5AK);
+        return alleVragen;
     }
 
-    public static ArrayList<Examens> getAllExamens(){
-        return naam;
+//    public static ArrayList<Examens> getAllExamens(ArrayList<Vraag>alleVragen){
+//        System.out.println();
+//        return naam;
     }
 
 
@@ -28,7 +41,7 @@ class Examens {
     // draagt bigfoot schoenmaat 420?
     // hoort bigfoot niet de naam te hebben bigfeet?
 
-}
+//}
 
 class Vraag {
     String vraag;
@@ -87,10 +100,18 @@ class Main {
         System.out.println("0) Exit");
         System.out.println("Kies een getal van het menu: ");
         int keuze = scanner.nextInt();
+
+        ArrayList<String> vragen = new ArrayList<>();
+        ArrayList<String> aardrijkskunde = new ArrayList<>();
+        aardrijkskunde.add("Is de aarde plat?");
+        Examens aardrijkskundeExamen = new Examens("Aardrijkskunde", 5, null);
+
+        ArrayList<Vraag> biologie = new ArrayList<>();
+
         switch (keuze) {
             case 0 -> System.out.println("Exiting....");
-            case 1 -> System.out.println("Monday");
-            Examens.
+            case 1 -> System.out.println(aardrijkskunde);
+
             case 2 -> System.out.println(Student.getAllStudents());
             case 3 -> System.out.println(Student.getAllStudents()); //bullshit
             case 4 -> System.out.println(Student.getAllStudents()); //bullshit
@@ -106,11 +127,7 @@ class Main {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> vragen = new ArrayList<>();
-        ArrayList<String> aardrijkskunde = new ArrayList<>();
-        aardrijkskunde.add("Is de aarde plat?");
 
-        ArrayList<Vraag> biologie = new ArrayList<>();
 
         System.out.println("Menu");
         menu();
