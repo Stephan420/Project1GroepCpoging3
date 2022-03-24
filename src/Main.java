547266a00e41dd79101abfbee3dc549102e61ee6
pimde <pimdeiters@gmail.com>
2022-03-24 15:35:43 +0100

    case 2

--- src/Main.java
+++ src/Main.java
@@ -132,7 +132,7 @@ class Student{
         return GeslaagdeExamens;
     }
 
-    public void createstudent(String naam,ArrayList<Examens> GeslaagdeExamens,ArrayList<Examens> NietGeslaagdeExamens){
+    public static void createstudent(String naam,ArrayList<Examens> GeslaagdeExamens,ArrayList<Examens> NietGeslaagdeExamens){
         studenten.add(new Student(naam,GeslaagdeExamens,NietGeslaagdeExamens));
     }
 
@@ -147,14 +147,17 @@ class Student{
 class Main {
     static Scanner scanner = new Scanner(System.in);
 
+
     //Hier worden de objecten van de examens aangemaakt
     static Examens aardrijkskundeExamen = new Examens("Aardrijkskunde", 5);
     static Examens biologieExamen = new Examens("Biologie", 5);
 
     static ArrayList<Examens> examens = new ArrayList<>();
 
-    static Student Jeroen = new Student("Jeroen",examens,examens);
-    static Student Pim = new Student("Pim",examens,examens);
+    Student Jeroen = new Student("Jeroen",examens,examens);
+    Student Pim = new Student("Pim",examens,examens);
+
+
 
     public static void menu(){
 
@@ -176,8 +179,8 @@ class Main {
         switch (keuze) {
             case 0 -> System.out.println("Exiting....");
             case 1 -> case1();
-            case 2 -> System.out.println(Student.getAllStudents());
-            case 3 -> System.out.println(Student.getAllStudents()); //bullshit
+            case 2 -> case2();
+            case 3 -> case3();
             case 4 -> System.out.println(Student.getAllStudents()); //bullshit
             case 5 -> case5();
             case 6 -> System.out.println(Student.getAllStudents()); //bullshit
@@ -192,6 +195,27 @@ class Main {
     public static void case1() {
         //Hier word de huidige lijst van de examens doorgegeven
         Examens.getAllExamens(aardrijkskundeExamen, biologieExamen);
+        menu();
+    }
+    public static void case2() {
+        if (Student.getAllStudents().size() == 0) {
+            System.out.println("Er zijn geen studenten");
+        }
+        else {
+            for (Student students:Student.getAllStudents()) {
+                System.out.println(students.naam);
+            }
+        }
+        menu();
+    }
+    public static void case3() {
+        System.out.println("Geef de naam van de student:");
+        String naam = scanner.nextLine();
+        Student.createstudent(naam,examens,examens);
+        //TODO dit nog ff fixen
+        System.out.println("Student aangemaakt");
+        System.out.println("Naam: " + naam);
+        System.out.println("Nummer: " + Student.studenten.get(Student.studenten.size()-1).nummer);
         menu();
     }
     public static void case5() {
@@ -212,5 +236,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Menu");
         menu();
+
     }
 }
